695b7b245bde6e21d83fd22348fe89d64737ca5d
Victor Costa <victor.costa@creditas.com.br>
2019-01-14 14:33:12 -0200

    [FEAT] Reset version

--- src/test/kotlin/refactoringexercises/smells/bloaters/longMethod/CalculatorTest.kt
+++ src/test/kotlin/refactoringexercises/smells/bloaters/longMethod/CalculatorTest.kt
@@ -9,37 +9,37 @@ class CalculatorTest {
 
     @Test
     fun `first month first day`() {
-        val result = calculator.calculateTotalAmount(100.0, 0.0, 1, 1)
+        val result = calculator.calculateTotalAmount(100.0, 0.0, MonthDate(1, 1))
         assertEquals(105.0, result, 0.0)
     }
 
     @Test
     fun `second month first day`() {
-        val result = calculator.calculateTotalAmount(100.0, 0.0, 2, 1)
+        val result = calculator.calculateTotalAmount(100.0, 0.0, MonthDate(2, 1))
         assertEquals(105.0, result, 0.0)
     }
 
     @Test
     fun `second month second day`() {
-        val result = calculator.calculateTotalAmount(100.0, 0.0, 2, 2)
+        val result = calculator.calculateTotalAmount(100.0, 0.0, MonthDate(2, 2))
         assertEquals(102.0, result, 0.0)
     }
 
     @Test
     fun `third month second day`() {
-        val result = calculator.calculateTotalAmount(100.0, 0.0, 3, 2)
+        val result = calculator.calculateTotalAmount(100.0, 0.0, MonthDate(3, 2))
         assertEquals(101.0, result, 0.0)
     }
 
     @Test
     fun `second month 25th day`() {
-        val result = calculator.calculateTotalAmount(100.0, 0.0, 2, 25)
+        val result = calculator.calculateTotalAmount(100.0, 0.0, MonthDate(2, 25))
         assertEquals(92.0, result, 0.0)
     }
 
     @Test
     fun `second month 30th day`() {
-        val result = calculator.calculateTotalAmount(100.0, 0.0, 2, 30)
+        val result = calculator.calculateTotalAmount(100.0, 0.0, MonthDate(2, 30))
         assertEquals(82.0, result, 0.0)
     }
 }
